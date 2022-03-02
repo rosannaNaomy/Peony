f68bd93d9eb1765578f66078c72cc88553cba95d
Naomy Portillo <naomyportillo@Naomys-MBP.fios-router.home>
2022-03-02 00:37:18 -0500

    setting up medicine display activity and layout

---
+++ app/src/main/java/com/example/peony/view/DisplayMedicineActivity.kt
@@ -0,0 +1,12 @@
+package com.example.peony.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.peony.R
+
+class DisplayMedicineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_medicine)
+    }
+}
