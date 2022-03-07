055dacd0548e01a6d2b3dff0c39d818f9be4d19e
Naomy Portillo <naomyportillo@Naomys-MBP.fios-router.home>
2022-03-06 23:21:21 -0500

    adjusted data insertion for db

--- app/src/main/java/com/example/peony/database/AppDao.kt
+++ app/src/main/java/com/example/peony/database/AppDao.kt
@@ -6,24 +6,20 @@ import androidx.room.Dao
 import androidx.room.Insert
 import com.example.peony.database.entities.MedicationData
 import com.example.peony.database.entities.UserEntity
-import com.example.peony.database.entities.relations.UserWithMedication
+import com.example.peony.database.entities.relations.UserwithMedications
 
 @Dao
 interface AppDao {
 
-    //pass entity, table name user, order by desc, new records will show on top
-//    @Query("SELECT * FROM user ORDER BY id DESC")
-//    fun getRecords(): List<UserEntity>
+    @Query("SELECT * FROM user")
+    fun getUser(): List<UserEntity>
 
-//    @Delete
-//    fun deleteUser(userEntity: UserEntity)
-//
-//    @Query("DELETE FROM user")
-//    fun deleteAllUsers()
+    @Delete
+    fun deleteUser(userEntity: UserEntity)
 
     //insert a record, which will take user input
-//    @Insert(onConflict = onConflictStrategy.REPLACE)
-//    fun insertUser(userEntity: UserEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertUser(userEntity: UserEntity)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMed(medicationData: MedicationData)
@@ -31,15 +27,11 @@ interface AppDao {
     @Query("DELETE FROM med")
     suspend fun deleteAllMeds()
 
-//    @Transaction
-//    @Query("SELECT * FROM user WHERE userName = :userName")
-//    suspend fun getUserAndMedWithUserName(userName: String): List<UserWithMedication>
+    @Transaction
+    @Query("SELECT * FROM user WHERE userName = :userName")
+    fun getUsersWithMedications(userName: String): List<UserwithMedications>
 
     @Query("SELECT * FROM med")
     fun getMeds(): LiveData<List<MedicationData>>
-
-//    @Transaction
-//    @Query("SELECT * FROM user WHERE userName = :userName")
-//    fun getUserWithUserMeds(userName: String): List<UserWithMedication>
 
 }
--- app/src/main/java/com/example/peony/model/Meta.kt
+++ app/src/main/java/com/example/peony/model/Meta.kt
@@ -4,6 +4,5 @@ data class Meta(
     val disclaimer: String,
     val last_updated: String,
     val license: String,
-    val results: Results,
     val terms: String
 )
